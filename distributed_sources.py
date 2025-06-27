a89a30cb77a26a03f6488a98d481403cb7de250d
Willian <willsbit@gmail.com>
2025-06-27 14:58:48 -0300

    formatting and project setup

--- distributed_sources.py
+++ distributed_sources.py
@@ -1,13 +1,12 @@
 import numpy as np
-import numpy_groupies as npg
 import polars as pl
 import matplotlib.pyplot as plt
 
 c0 = 343  # [m/s]
 rho0 = 1.2  # [kg/m^3]
 p_ref = 2e-5  # [Pa]
-a = 28e-3 / 2 # [m]
-r = 1 # [m]
+a = 28e-3 / 2  # [m]
+r = 1  # [m]
 
 elements_schema = {
     "node1": pl.UInt32,
@@ -20,13 +19,15 @@ elements = pl.read_csv(
     has_header=False,
     new_columns=["node1", "node2", "node3"],
 ).cast(elements_schema)
-nodes = pl.read_csv(
-    "distributed_source_data/nodes.txt",
-    separator=",",
-    has_header=False,
-    new_columns=["x", "y", "z"],
-).with_row_index(name="node", offset=1).with_columns(
-    pl.col("x", "y", "z") / 10
+nodes = (
+    pl.read_csv(
+        "distributed_source_data/nodes.txt",
+        separator=",",
+        has_header=False,
+        new_columns=["x", "y", "z"],
+    )
+    .with_row_index(name="node", offset=1)
+    .with_columns(pl.col("x", "y", "z") / 10)
 )
 
 velocity = pl.read_csv(
@@ -69,32 +70,23 @@ elements_with_coordinates = (
 
 field_points = pl.DataFrame({"x": [0], "y": [0], "z": [1]}).with_row_index(name="field_point_id")
 
-elements_with_euclidean_distances = (
-    elements_with_coordinates
-    .join(field_points, how="cross")
-    .select(
-        pl.col("field_point_id", "area"),
-        (
+elements_with_euclidean_distances = elements_with_coordinates.join(field_points, how="cross").select(
+    pl.col("field_point_id", "area"),
+    (
         np.sqrt(
-            (pl.col("x") - pl.col("x_bar")) ** 2 +
-            (pl.col("y") - pl.col("y_bar")) ** 2 +
-            (pl.col("z") - pl.col("z_bar")) ** 2)
-        ).alias("distance"),
-    )
+            (pl.col("x") - pl.col("x_bar")) ** 2
+            + (pl.col("y") - pl.col("y_bar")) ** 2
+            + (pl.col("z") - pl.col("z_bar")) ** 2
+        )
+    ).alias("distance"),
 )
 
-velocities_per_frequency = (
-    velocity
-    .with_columns(
-        (pl.col("frequency") * 2 * np.pi).alias("omega"),
-        ((pl.col("frequency") * 2 * np.pi) / c0).alias("k")
-    )
-    .sort(by="frequency")
-)
+velocities_per_frequency = velocity.with_columns(
+    (pl.col("frequency") * 2 * np.pi).alias("omega"), ((pl.col("frequency") * 2 * np.pi) / c0).alias("k")
+).sort(by="frequency")
 
 rayleigh_integral_per_frequency = (
-    elements_with_euclidean_distances
-    .join(velocities_per_frequency, how="cross")
+    elements_with_euclidean_distances.join(velocities_per_frequency, how="cross")
     .with_columns(((pl.col("omega") * pl.col("u") * pl.col("area")) / pl.col("distance")).alias("mul_factors"))
     .select(
         pl.col("field_point_id", "frequency"),
@@ -105,28 +97,37 @@ rayleigh_integral_per_frequency = (
     .sum()
     .with_columns(
         # multiply by -j*rho0 / 2pi
-        (pl.col("re") * (-rho0 / (2*np.pi))).alias("im"),
-        (pl.col("im") * (rho0 / (2*np.pi))).alias("re"),
+        (pl.col("re") * (-rho0 / (2 * np.pi))).alias("im"),
+        (pl.col("im") * (rho0 / (2 * np.pi))).alias("re"),
     )
 )
 
-pressure_per_frequency = (
-    rayleigh_integral_per_frequency
-    .select(pl.col("frequency"),
-    (10 * ((0.5 * (pl.col("re").pow(2) + pl.col("im").pow(2))) / (p_ref**2)).log(base=10)).alias("Lp")
-    )
-    .sort(by="frequency")
-)
+pressure_per_frequency = rayleigh_integral_per_frequency.select(
+    pl.col("frequency"),
+    (10 * ((0.5 * (pl.col("re").pow(2) + pl.col("im").pow(2))) / (p_ref**2)).log(base=10)).alias("Lp"),
+).sort(by="frequency")
 pressure_per_frequency
 
 # theoretical_model
 velocities_array = velocities_per_frequency.sort(by="frequency").to_numpy(structured=True)
 
 velocities_array
-far_field_pressure = -1j*0.5*rho0*c0*velocities_array["u"] * velocities_array["k"] * a * (a/r) * np.exp(1j*velocities_array["k"]*r)
-far_field_Lp =  10 * np.log10((0.5*np.real(far_field_pressure * np.conj(far_field_pressure))) / (p_ref**2))
+far_field_pressure = (
+    -1j
+    * 0.5
+    * rho0
+    * c0
+    * velocities_array["u"]
+    * velocities_array["k"]
+    * a
+    * (a / r)
+    * np.exp(1j * velocities_array["k"] * r)
+)
+far_field_Lp = 10 * np.log10((0.5 * np.real(far_field_pressure * np.conj(far_field_pressure))) / (p_ref**2))
 
 fig, ax = plt.subplots()
 ax.semilogx(velocities_array["frequency"], far_field_Lp, label="Apoximação campo distante", linestyle="--")
-ax.semilogx(pressure_per_frequency.select("frequency"), pressure_per_frequency.select("Lp"), label="Simulado", alpha=0.5)
+ax.semilogx(
+    pressure_per_frequency.select("frequency"), pressure_per_frequency.select("Lp"), label="Simulado", alpha=0.5
+)
 ax.legend()
